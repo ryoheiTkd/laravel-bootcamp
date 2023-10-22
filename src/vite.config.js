b64cb96d040421143dbad325a1a32c435a08c1bb
Ryohei Takada <takada@althaea.info>
2023-10-22 16:59:20 +0900

    install breeze and host via Vite server

--- src/vite.config.js
+++ src/vite.config.js
@@ -2,9 +2,24 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server: {
+        host: true,
+        // クライアントはlocalhost:8080でnginxのコンテナにアクセスする
+        // その後ブラウザからlocalhost:5173/resources…と呼び出されるためこちらでもホストを指定する必要があった
+        hmr: {
+            host: 'localhost',
+        },
+        // WSLのファイル編集イベントをキャッチできない既知のエラー
+        watch: {
+            usePolling: true,
+        },
+    },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
     ],
